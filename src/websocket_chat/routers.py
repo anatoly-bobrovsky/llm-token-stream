63bba77475313c3c4d0d9212eb72cef458d43d35
anatoly-bobrovsky <bobrovsky.anatoliy@gmail.com>
2024-11-09 14:43:45 +0600

    fix: async output

--- src/websocket_chat/routers.py
+++ src/websocket_chat/routers.py
@@ -2,9 +2,11 @@
 
 import asyncio
 
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, WebSocketException, status
+from pydantic import ValidationError
 
-from .bl import MessageChunksQueue, llm_stream
+from .bl import llm_stream
+from .schemas import Message
 
 websocket_chat_router = APIRouter(
     tags=["chat"],
@@ -13,27 +15,20 @@ websocket_chat_router = APIRouter(
 
 @websocket_chat_router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    """Handle incoming WebSocket connections and messages.
+    """
+    Handle incoming WebSocket connections and messages.
 
     Args:
         websocket (WebSocket): The WebSocket connection object.
     """
     await websocket.accept()
 
-    message_priority = 0
-    queue = MessageChunksQueue()
-
     try:
         while True:
-            user_message = await websocket.receive_text()
+            user_message = Message.parse_raw(await websocket.receive_text())
+            asyncio.create_task(llm_stream(user_message=user_message, websocket=websocket))
 
-            asyncio.create_task(
-                llm_stream(
-                    user_message=user_message, websocket=websocket, message_priority=message_priority, queue=queue
-                )
-            )
-
-            message_priority += 1
-
+    except ValidationError:
+        raise WebSocketException(code=status.WS_1003_UNSUPPORTED_DATA)
     except WebSocketDisconnect:
         pass
